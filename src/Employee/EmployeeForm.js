542058e446087f6752c64fa9f185bd1099dbb80e
Pedro Arvellos <pedro.goncalves@watchguard.com>
2019-11-19 21:48:23 -0300

    Create form state handling

--- src/Employee/EmployeeForm.js
+++ src/Employee/EmployeeForm.js
@@ -3,18 +3,20 @@ import { Paper, TextField, Button, Box } from '@material-ui/core'
 
 const EmployeeForm = props => {
     const [info, setInfo] = useState({})
+
     const handleChange = (e, fieldName) => {
-        setInfo(...info, ...{ [fieldName]: e.target.value })
+        setInfo({ ...info, ...{ [fieldName]: e.target.value } })
     }
     return (
         <Paper style={{ margin: '1em 0', padding: '0 2em' }}>
             <form onSubmit={e => console.log(e)} style={{ width: '100%', padding: '0.5em' }}>
                 <TextField
-                    label='role'
+                    label='name'
                     margin='normal'
-                    value={info.role ? info.role : ''}
-                    onChange={event => handleChange(event, 'role')}
+                    value={info.name ? info.name : ''}
+                    onChange={event => handleChange(event, 'name')}
                     fullWidth
+                    autoFocus
                 />
             </form>
         </Paper>
